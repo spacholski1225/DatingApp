5e8809f58c368b6a7566096482a1cff89ad8cdae
spacholski1225 <spacholski1225@gmail.com>
2020-12-06 21:22:31 +0100

    added HTTP requests, CORS support and displaying fetched users in browser

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,10 +36,12 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
