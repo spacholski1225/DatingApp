808fac8fbaa2644b17961773d62935800390ac44
spacholski1225 <spacholski1225@gmail.com>
2020-12-08 22:23:55 +0100

    added new controllers responsible for users password

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
