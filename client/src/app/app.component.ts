15f17c30b804a3c1020ed6e3bc723840d1f2b8d1
spacholski1225 <spacholski1225@gmail.com>
2020-12-15 13:18:23 +0100

    added register cancel funcitionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,11 +11,11 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit{
   title = 'The Dating App';
   users: any;
-  constructor(private http: HttpClient, private accountService: AccountService) {
+  constructor(private accountService: AccountService) {
 
   }
   ngOnInit() {
-    this.getUsers();
+
     this.setCurrentUser();
   }
 
@@ -24,11 +24,5 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
+  
 }
